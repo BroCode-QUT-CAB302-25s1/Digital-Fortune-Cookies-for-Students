b60d1030209452ce336ed98a1c601a6079d87e21
Antares <overlimit090@gmail.com>
2025-04-08 17:16:49 +1000

    add icon

---
+++ src/main/java/com/example/project/ProfileScreen.java
@@ -0,0 +1,17 @@
+
+package com.example.project;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ProfileScreen extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/project/ProfileScreenController.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class ProfileScreenController {
+
+
+}
