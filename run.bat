71ab9846cde93d94473968e07e2234c595eb198f
Antares <overlimit090@gmail.com>
2025-04-28 19:28:02 +1000

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,6 @@
+(
+    echo "===== Commit Summary ====="
+    git shortlog --summary --numbered --all --no-merges
+    echo -e "\n\n===== Detailed Commit Log ====="
+    git log --pretty=format:'%h was %an, %ar, message: %s'
+) > git_commit_report.txt
