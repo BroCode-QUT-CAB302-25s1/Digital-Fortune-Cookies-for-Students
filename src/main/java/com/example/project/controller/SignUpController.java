67983a21fd20f47a76413ae21ec97b0fcd0c1eb8
Brian-brocode <brianchiu74@hotmail.com>
2025-04-30 21:37:21 +1000

    added security question containers

--- src/main/java/com/example/project/controller/SignUpController.java
+++ src/main/java/com/example/project/controller/SignUpController.java
@@ -9,10 +9,7 @@ import com.example.project.util.ErrorAlert;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
@@ -35,6 +32,12 @@ public class SignUpController {
 
     @FXML
     private Button backButton;
+
+    @FXML
+    public ChoiceBox securityQuestion;
+
+    @FXML
+    public TextField securityAnswer;
 
     @FXML
     private Label titleLabel;
@@ -77,6 +80,17 @@ public class SignUpController {
 
     @FXML
     private void initialize() {
+        // Initialise
+        securityQuestion.getItems().addAll("What is your mother's maiden name?",
+                "What was the name of your first pet?",
+                "What is your oldest sibling’s middle name?",
+                "What was the make of your first car?",
+                "What is your favorite childhood teacher's name?",
+                "What city were you born in?",
+                "What is your high school mascot?",
+                "What was the name of your childhood imaginary friend?",
+                "What is the name of your favorite book as a child?",
+                "What is the title of the first movie you remember seeing in a cinema?");
         // Optional: Initialize UI components, e.g., set default values or styles
         signupButton.setOnAction(this::handleSignupButton);
         backButton.setOnAction(this::handleBackButton);
