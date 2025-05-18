7f75b5b6ecb0117901bedb73dcea377d4fc36192
Antares <overlimit090@gmail.com>
2025-05-19 01:46:49 +1000

    modify git log

--- gitCommitLog_ShortReport.bat
+++ gitCommitLog_ShortReport.bat
@@ -2,5 +2,5 @@
     	echo "===== Commit Summary ====="
 	git shortlog --summary --numbered --all --no-merges
     	echo -e "===== Detailed Commit Log ====="
-    	git log --pretty=format:'%h was %an (%ae), %at, %ar, message: %s'
+    	git log --pretty=format:'%h was %an , %ar, message: %s'
 ) > git_commit_report.txt
