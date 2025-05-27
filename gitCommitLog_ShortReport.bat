540cf98319ecee2f8801e6f2793483ac367a5c7a
Antares <overlimit090@gmail.com>
2025-05-27 18:23:40 +1000

    update gitCommit report

--- gitCommitLog_ShortReport.bat
+++ gitCommitLog_ShortReport.bat
@@ -1,6 +1,4 @@
 (
-    	echo "===== Commit Summary ====="
+    	echo "===== Commit Summary (no merges) ====="
 	git shortlog --summary --numbered --all --no-merges
-    	echo -e "===== Detailed Commit Log ====="
-    	git log --pretty=format:'%h was %an , %ar, message: %s'
 ) > git_commit_report.txt
